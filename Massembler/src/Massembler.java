9c25e5cd56050aab59815bd6ce60dad44768dad8
Mohamed Aesawy <m.m.aesawy@gmail.com>
2019-12-16 17:18:54 +0200

    Updated instructions. Memory impl

--- Massembler/src/Massembler.java
+++ Massembler/src/Massembler.java
@@ -7,65 +7,77 @@ public class Massembler {
     //array of registers
     Register[] regs = new Register[32];
 
+
     //program counter in order to help us jumping in the program
-    int programCounter;
+    int programCounter = 0;
 
     public void add(Register rd, Register rs, Register rt){
         rd.value = rs.value + rt.value;
+        programCounter+=1;
     }
+
     public void sub(Register rd, Register rs, Register rt){
         rd.value = rs.value - rt.value;
+        programCounter+=1;
     }
-    public int slt(Register rs, Register rt){
 
-        return rs.value < rt.value ? 1:0;
+    public void slt(Register rs, Register rt){
+        programCounter+=1;
+        //rs.value < rt.value ? 1:0;
     }
-    public int and(Register rd, Register rs, Register rt){
-        rd.value = 0;
-        if(rs.value == rt.value && rs.value != 0)
-            rd.value = 1;
-        return  rd.value;
-    }
-    public int slti(Register rs, int imm){
 
-        return rs.value < imm ? 1:0;
-    }
-    public int or(Register rd, Register rs, Register rt){
+    public void and(Register rd, Register rs, Register rt){
+        programCounter+=1;
+        rd.setNumber(rs.getNumber() & rt.getNumber());
 
-        rd.value = 0;
-        if(rs.value == 1 || rt.value == 1)
-            rd.value = 1;
-        return rd.value;
+    }
+
+    public void slti(Register rs, int imm){
+        //if $s < $t $d = 1
+        Register t = null;
+        if(rs.getNumber() < imm)
+            //t = 1
+            t.setNumber(1);
+        else{
+            t.setNumber(0);
+        }
+        programCounter+=1;
+
+    }
+
+    public void or(Register rd, Register rs, Register rt){
+        programCounter+=1;
+        rd.setNumber(rs.getNumber() | rt.getNumber());
     }
 
     public void addi(Register rd, Register rs, int imm){
-
+        programCounter+=1;
         rd.value = rs.value + imm;
     }
-    public void subi(Register rd, Register rs, int imm){
 
-        rd.value = rs.value - imm;
+    public void andi(Register rd, Register rs, int imm){
+        programCounter+=1;
+
+        rd.setNumber(rs.getNumber() & imm);
+
     }
 
-    public int andi(Register rd, Register rs, int imm){
-
-        rd.value = 0;
-        if(rs.value == imm && rs.value != 0)
-            rd.value = 1;
-        return  rd.value;
-    }
-    public int ori(Register rd, Register rs, int imm){
-
-        rd.value = 0;
+    public void ori(Register rd, Register rs, int imm){
+        programCounter+=1;
+        rd.setNumber(1);
         if(rs.value == 1 || imm == 1)
-            rd.value = 1;
-        return rd.value;
-    }
-    public void lw(Register rs, int address){
+            rd.setNumber(1);
 
     }
-    public void sw(Register rs, int address){
 
+    public void lw(Register reg1, int addr){
+
+        reg1.setNumber(mem.get(addr));
+    }
+
+    public void sw(Register reg1, int address){
+
+        mem.set(address, reg1.getNumber());
     }
 
 }
